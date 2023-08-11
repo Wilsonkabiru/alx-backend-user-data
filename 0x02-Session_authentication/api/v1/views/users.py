9345a9fed84696b568c1a554a1b63daf47c2f6d6
Carlos Wilson Niqx <wkabiru19@gmail.com>
2023-08-11 13:27:33 +0300

    Update users.py

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -114,7 +114,7 @@ def update_user(user_id: str = None) -> str:
       - 404 if the User ID doesn't exist
       - 400 if can't update the User
     """
-    if user_id is None:
+    if user_id is None:\n
         abort(404)
     user = User.get(user_id)
     if user is None:
